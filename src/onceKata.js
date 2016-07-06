0757b7965346fe244b98c6951a2d65d1b1f33489
Dylan T. Moberg <dylanmoberg@msn.com>
2016-07-06 00:36:05 -0700

    initial commit

---
+++ src/onceKata.js
@@ -0,0 +1,15 @@
+/*Description:
+
+You'll implement once, a function that takes another function as an argument, and returns a new version of that function that can only be called once.
+
+Subsequent calls to the resulting function should have no effect (and should return undefined).*/
+
+function once(fn) {
+  var call = true;
+  return function() {
+    if (call) {
+      call=false;
+      return fn.apply(this,arguments);
+    }
+  };
+}
--- src/test.js
+++
@@ -1,9 +0,0 @@
-function once(fn) {
-  var call = true;
-  return function() {
-    if (call) {
-      call=false;
-      return fn.apply(this,arguments);
-    }
-  };
-}
